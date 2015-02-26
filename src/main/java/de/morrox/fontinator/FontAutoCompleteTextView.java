e828db6058f71e1e2ef8f6363642fae4090710cd
Sven Naehler <sven.naehler@9elements.com>
2015-02-26 15:40:47 +0100

    Fix: FontAutoCompleteTextView extends wrong Widget

--- src/main/java/de/morrox/fontinator/FontAutoCompleteTextView.java
+++ src/main/java/de/morrox/fontinator/FontAutoCompleteTextView.java
@@ -3,12 +3,12 @@ package de.morrox.fontinator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Pair;
-import android.widget.RadioButton;
+import android.widget.AutoCompleteTextView;
 
 import de.morrox.fontinator.utilities.TypefaceLoader;
 import de.morrox.fontinator.utilities.Typefaceable;
 
-public class FontAutoCompleteTextView extends RadioButton implements Typefaceable {
+public class FontAutoCompleteTextView extends AutoCompleteTextView implements Typefaceable {
     private TypefaceLoader typefaceLoader;
     public FontAutoCompleteTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
