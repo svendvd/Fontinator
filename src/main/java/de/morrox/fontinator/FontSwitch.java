40cfb2a569d9382dd3231b4a9a00448e48546139
Sven Naehler <sven.naehler@9elements.com>
2015-02-26 15:39:42 +0100

    Add FontSwitch

---
+++ src/main/java/de/morrox/fontinator/FontSwitch.java
@@ -0,0 +1,27 @@
+package de.morrox.fontinator;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Pair;
+import android.widget.Switch;
+
+import de.morrox.fontinator.utilities.TypefaceLoader;
+import de.morrox.fontinator.utilities.Typefaceable;
+
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+public class FontSwitch extends Switch implements Typefaceable {
+    private TypefaceLoader typefaceLoader;
+    public FontSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        typefaceLoader = TypefaceLoader.get(this, context, attrs);
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        Pair<CharSequence, BufferType> pair = TypefaceLoader.inject(typefaceLoader, text, type);
+        super.setText(pair.first, pair.second);
+    }
+}
